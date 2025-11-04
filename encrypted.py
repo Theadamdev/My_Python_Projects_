c6a6a4a358993d22b2f3762bd680818c0424268b
Theadamdev <adamsabri.student@gmail.com>
2025-11-04 18:49:10 +0100

    Add files via upload

---
+++ encrypted.py
@@ -0,0 +1,27 @@
+import random
+import string
+
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+key =  chars.copy()
+random.shuffle(key)
+
+#now the encryption
+message = input("enter a message to encrypt : ")
+encrypted = ""
+for letter in message:
+    index = chars.index(letter)
+    encrypted += key[index]
+
+print(f"Your message was : {message}")
+print(f"Your encrypted message is : {encrypted}")
+
+#now the decryption
+encrypted = input("enter a message to decrypt : ")
+message = ""
+for letter in encrypted:
+    index = key.index(letter)
+    message += chars[index]
+
+print(f"Your encrypted message was : {encrypted}")
+print(f"Your message is : {message}")
