afa924f531027bd6634f3c7207a0fcbd421352a0
Theadamdev <adamsabri.student@gmail.com>
2025-11-05 15:34:28 +0100

    Add files via upload

---
+++ hangman/words.py
@@ -0,0 +1,12 @@
+words = (
+    "apple", "banana", "orange", "grape", "melon", "pear", "peach", "cherry", "mango", "lemon",
+    "table", "chair", "window", "door", "floor", "ceiling", "lamp", "sofa", "carpet", "desk",
+    "dog", "cat", "bird", "fish", "horse", "cow", "sheep", "lion", "tiger", "bear",
+    "sun", "moon", "star", "sky", "cloud", "rain", "snow", "wind", "storm", "tree",
+    "book", "pen", "pencil", "paper", "bag", "clock", "phone", "computer", "mouse", "keyboard",
+    "shirt", "pants", "shoes", "hat", "coat", "dress", "scarf", "gloves", "belt", "socks",
+    "car", "bus", "train", "plane", "boat", "bike", "truck", "subway", "taxi", "helicopter",
+    "red", "blue", "green", "yellow", "black", "white", "purple", "pink", "brown", "gray",
+    "water", "juice", "milk", "coffee", "tea", "bread", "rice", "egg", "cheese", "butter",
+    "house", "school", "city", "village", "river", "mountain", "beach", "forest", "garden", "road"
+)
